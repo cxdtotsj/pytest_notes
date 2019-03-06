982186fb6b896c8c3b80d60449483ec44e469a01
cxdtotsj <cxdtotsj@163.com>
2019-03-06 12:57:22 +0800

    fixture==3.6

---
+++ ch3/b/tasks_proj/tests/conftest.py
@@ -0,0 +1,60 @@
+import pytest
+import tasks
+from tasks import Task
+
+
+# 将 task_db 调整至 session 级别
+@pytest.fixture(scope='session')
+def tasks_db_session(tmpdir_factory):
+    """Connect to db before tests, disconncet after."""
+
+    temp_dir = tmpdir_factory.mktemp('temp')
+    tasks.start_tasks_db(str(temp_dir), 'tiny')
+    yield
+    tasks.stop_tasks_db()
+
+
+@pytest.fixture()
+def tasks_db(tasks_db_session):
+	tasks.delete_all()
+
+
+@pytest.fixture(scope='session')
+def tasks_just_a_few():
+
+    return (
+        Task('Write some code', 'Brian', True),
+        Task("Code review Brian's code", 'Katie', False),
+        Task('Fix what Brian did', 'Michelle', False)
+    )
+
+@pytest.fixture(scope='session')
+def tasks_mult_per_owner():
+
+    return (
+        Task('Make a cookie', 'Raphael'),
+        Task('Use an emoji', 'Raphael'),
+        Task('Move to Berlin', 'Raphael'),
+
+        Task('Create', 'Michelle'),
+        Task('Inspire', 'Michelle'),
+        Task('Encourage', 'Michelle'),
+
+        Task('Do a handstand', 'Daniel'),
+        Task('Write some books', 'Daniel'),
+        Task('Eat ice cream', 'Daniel')
+    )
+
+@pytest.fixture()
+def do_with_3_tasks(tasks_db, tasks_just_a_few):
+    """Connected db with 3 tasks, all unique."""
+
+    for t in tasks_just_a_few:
+        tasks.add(t)
+
+@pytest.fixture()
+def do_with_multi_per_owner(tasks_db, tasks_mult_per_owner):
+    """Connected db with 9 tasks, 3 owner, all with 3 tasks."""
+
+    for t in tasks_mult_per_owner:
+        tasks.add(t)
